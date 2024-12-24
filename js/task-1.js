c39c13011180aaa515167874db00e871fe628bd4
denis-cactus <m.play.sys@gmail.com>
2024-12-24 21:44:33 +0200

    task-1 done

--- js/task-1.js
+++ js/task-1.js
@@ -1,19 +1,21 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  let totalPrice = quantity * pricePerDroid;
+// function calculateTotalPrice(order) {
+//   let totalPrice = 0;
+//   for (let item of order) {
+//     totalPrice += item;
+//   }
+//   return totalPrice;
+// }
+// console.log(calculateTotalPrice([10, 25, 56, 45]));
 
-  if (quantity === 0) {
-    return 'You did not order any droids.';
-  }
-
-  if (totalPrice > customerCredits) {
-    return 'Insufficient funds!';
-  } else {
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-  }
+// let num = 10;
+// for (let i = 0; i <= num; i++) {
+//   console.log(i);
+// }
+function slugify(text) {
+  return text.toLowerCase().trim().replace(/\s+/g, '-');
 }
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
-console.log(makeTransaction(0, 3000, 23000)); // "You did not order any droids."
+
+console.log(slugify('Arrays for beginners')); // "arrays-for-beginners"
+console.log(slugify('English for developer')); // "english-for-developer"
+console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
+console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,54 @@
-function checkForSpam(message) {
-  return (
-    message.toLowerCase().includes('sale') ||
-    message.toLowerCase().includes('spam')
-  );
+// // function sum(a, b) {
+// //   console.log(arguments);
+// //   return a + b;
+// // }
+
+// // sum(2, 5);
+// function sum() {
+//   let total = 0;
+//   for (let i = 0; i < arguments.length; i++) {
+//     total += arguments[i];
+//   }
+//   return total;
+// }
+// console.log(sum(3, 5)); // 8
+// console.log(sum(1, 2, 3, 4, 5, 8, 9, 65, 423));
+
+// function createReversedArray() {
+//   const args = Array.from(arguments);
+//   return args.reverse();
+// }
+// console.log(createReversedArray(1, 2, 3));
+/*
+ *   Функція calculateTax(amount, taxRate) оголошує два параметри:
+ *   amount - число, сума від якої потрібно обчислити податок. Обов'язковий параметр.
+ *   taxRate - число, податкова ставка. Необов'язковий параметр. За замовчуванням його значення має бути 0.2.
+ *   Доповни код функції так, щоб вона повертала суму податку - результат множення суми на податкову ставку.
+
+ */
+// function calculateTax(amount, taxRate = 0.2) {
+//   return amount * taxRate;
+// }
+// console.log(calculateTax(100, 0.1));
+// const multiply = function (x, y, z) {
+//   console.log(x * y * z);
+// };
+// console.log(function calculateTax(amount, taxRate = 0.2) {
+//   return amount * taxRate;
+// }
+// console.log(calculateTax(100, 0.1));
+function bar() {
+  console.log('bar');
 }
 
-console.log(checkForSpam('Latest technology news')); // false
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
-console.log(checkForSpam('Get best sale offers now!')); // true
-console.log(checkForSpam('Amazing SalE, only tonight!')); // true
-console.log(checkForSpam('Trust me, this is not a spam message')); // true
-console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+function baz() {
+  console.log('baz');
+}
+
+function foo() {
+  console.log('foo');
+  bar();
+  baz();
+}
+
+foo();
